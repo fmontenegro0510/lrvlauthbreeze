fd39171054b6ae7a521dbeb8a81839a292d18238
Francisco Montenegro <franciscomontenegro22@gmail.com>
2022-05-10 09:55:44 -0300

    protect default route, and migration

--- routes/web.php
+++ routes/web.php
@@ -12,10 +12,13 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/', function () {
+Route::middleware(['auth'])->get('/', function () {
     return view('welcome');
-});
+})->name('welcome');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
